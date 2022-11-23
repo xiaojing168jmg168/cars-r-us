ef802fb8562e085add34304335ad3cdee5d12495
Justin Greene <justingreene@iMac.attlocal.net>
2022-11-23 18:46:22 -0500

    fix models

--- controllers/index.js
+++ controllers/index.js
@@ -4,8 +4,8 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,20 +1,12 @@
 const User = require('./User');
-const Blog = require('./blog');
-const Comment = require('./comment');
+const Car = require('./Car')
 
-User.hasMany(Blog, {
-  foreignKey: 'blog_id',
+User.hasMany(Car, {
+  foreignKey: 'car_id',
 });
 
-User.hasMany(Comment, {
-  foreignKey: 'comment_id',
-});
-
-Blog.belongsTo(User, {
+Car.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-Comment.belongsTo(User, {
-  foreignKey: 'user_id',
-});
-module.exports = { User, Blog, Comment };
+module.exports = { User, Car };
