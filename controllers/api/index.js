4c42eb438c082af6374681e8e74b0cc66a9a4124
Daniel Vidal <daniel.vidal17@yahoo.com>
2022-11-28 18:00:04 -0500

    updated api index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,7 @@ const {User, Car} = require('../../models')
 
 const userRoutes = require('./user-routes');
 const carRoutes = require('./car-routes');
+
 router.use('/users', userRoutes);
-router.use('/cars', userRoutes);
+router.use('/cars', carRoutes);
 module.exports = router;
